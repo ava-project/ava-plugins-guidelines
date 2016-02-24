6aaa81d6e9622c4c8975187fab6444f5a363d45b
Tommy <thomas.milox@epitech.eu>
2016-02-24 15:51:55 +0100

    [test] 	protobuf: 	- started basics tests. 	- started serialization tests.

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,44 @@
 #include "ava_api.pb.h"
 
-int main() {
+#include <assert.h>
+#include <iostream>
+#define NDEBUG
+
+
+void basic() {
   api::Plugin plugin;
+
+  assert(plugin.GetDescriptor());
+
+  plugin.set_name("firefox");
+  plugin.set_command("start firefox");
+
+  std::cout << plugin.name() << std::endl;
+  std::cout << plugin.command() << std::endl;
+
+  assert(plugin.name() == "firefox");
+  assert(plugin.command() == "start firefox");
+}
+
+void serialization() {
+  std::string s1;
+  std::string s2;
+  api::Plugin plugin;
+  api::Plugin result;
+
+  plugin.set_name("google-chrome");
+  plugin.set_name("start google-chrome");
+  plugin.SerializeToString(&s1);
+  std::cout << s1 << std::endl;
+  result.ParseFromString(s1);
+  result.SerializeToString(&s2);
+  std::cout << s2 << std::endl;
+  assert(s1 == s2);
+}
+
+int main() {
+
+  basic();
+  serialization();
+  return 0;
 }
