4e7b66183b08e40d390d019125f6e3107e1f827c
Tommy <thomas.milox@epitech.eu>
2016-11-21 20:35:28 +0100

    [server] 	- fixed issue when the socket accepted was destroyed before I/O operations 	- server contains tcp clients accepted. 	- Use of std::shared_ptr to keep the socket alive
    [client]
    	- tcp client class implemented.

--- c++/server/include/Netlib.hpp
+++ c++/server/include/Netlib.hpp
@@ -2,16 +2,24 @@
 
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <atomic>
+#include <chrono>
 #include <cstring>
+#include <ctime>
 #include <functional>
+#include <iomanip>
 #include <iostream>
+#include <map>
+#include <memory>
+#include <mutex>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <thread>
 #include <vector>
 
 namespace netlib {
@@ -25,6 +33,25 @@ namespace tools {
 
 static unsigned int const BACKLOG = 30;
 static unsigned int const BUFFER_SIZE = 8096;
+
+template <typename T>
+std::size_t hash(const T &to_hash) {
+  std::hash<T> h;
+  return h(to_hash);
+}
+
+std::string get_current_time_and_date() {
+  std::mutex mutex;
+  std::stringstream ss;
+
+  std::lock_guard<std::mutex> lock(mutex);
+  {
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+  }
+  return ss.str();
+}
 
 }  //! namespace tools
 
@@ -41,7 +68,7 @@ class socket {
   socket(void)
       : fd_(-1),
         host_("127.0.0.1"),
-        port_(12345),
+        port_(0),
         addrinfo_({0}),
         v_addrinfo_({0}),
         is_socket_bound_(false) {}
@@ -62,7 +89,7 @@ class socket {
   socket &operator=(const socket &) = default;
 
   //! dtor
-  ~socket(void) { close(); }
+  ~socket(void) {}
 
  public:
   //!
@@ -179,6 +206,7 @@ class socket {
 
     switch (bytes_read) {
       case -1:
+        ::perror("error: ");
         __RUNTIME_ERROR__("tcp::socket::receive: recv() failed.");
         break;
       case 0:
@@ -213,7 +241,7 @@ class socket {
   const std::string &get_host() const { return host_; }
 
   //! get the socket port
-  int get_port() const { return port_; }
+  unsigned int get_port() const { return port_; }
 
   //! returns true or false whether the socket is bound
   bool is_socket_bound() const { return is_socket_bound_; }
@@ -278,6 +306,77 @@ class socket {
 };
 
 //!
+//! tcp client
+//!
+class client {
+ public:
+  //! ctor
+  client(void) : is_connected_(false) {}
+
+  //! creates a client from an existing socket
+  explicit client(const socket &socket)
+      : socket_(socket), is_connected_(true) {}
+
+  //! copy ctor
+  client(const client &) = delete;
+
+  //! assignment operator
+  client &operator=(const client &) = delete;
+
+  //! dtor
+  ~client(void) { disconnect(); }
+
+ public:
+  //! connects the client to the given endpoint
+  void connect(const std::string &host, unsigned int port) {
+    if (is_connected_)
+      __LOGIC_ERROR__("tcp::client::connect: Client is already connected.");
+
+    socket_.connect(host, port);
+    is_connected_ = true;
+  }
+
+  //! sends amount of data
+  std::size_t send(const std::string &message) {
+    return send(std::vector<char>(message.begin(), message.end()),
+                message.size());
+  }
+
+  //! sends amount of data
+  std::size_t send(const std::vector<char> &data, std::size_t data_len) {
+    if (not is_connected_)
+      __LOGIC_ERROR__("tcp::client::send: Client must be connected.");
+
+    return socket_.send(data, data_len);
+  }
+
+  //! receives amount of data
+  std::vector<char> receive(std::size_t size_to_read) {
+    if (not is_connected_)
+      __LOGIC_ERROR__("tcp::client::receive: Client must be connected.");
+    return socket_.receive(size_to_read);
+  }
+
+  //! disconnects the client
+  void disconnect() {
+    if (not is_connected_) return;
+
+    socket_.close();
+    is_connected_ = false;
+  }
+
+  //! returns true/false whether the client is already connected
+  bool is_connected() const { return is_connected_; }
+
+ private:
+  //! client socket
+  socket socket_;
+
+  //! boolean to know if the client is already connected
+  std::atomic_bool is_connected_;
+};
+
+//!
 //! tcp server
 //!
 
@@ -296,13 +395,14 @@ class server {
   ~server(void) { stop(); }
 
  public:
-  //! set callback
+  //! triggers a callback when a new client is accepted
   void on_accept(
-      const std::function<void(const std::string &, socket &)> &callback) {
+      const std::function<void(const std::string &, std::shared_ptr<client> &)>
+          &callback) {
     callback_ = callback;
   }
 
-  // starts the server
+  //! starts the server
   void run(const std::string &host, unsigned int port) {
     if (is_running_)
       __RUNTIME_ERROR__("tcp::server::run: Server is aldready running.");
@@ -318,21 +418,38 @@ class server {
     if (not is_running_) return;
 
     socket_.close();
+    for (auto &client : clients_) {
+      client.second->disconnect();
+    }
+    clients_.clear();
     is_running_ = false;
   }
+
+  //! returns true/false whether the server is running
+  bool is_running() const { return is_running_; }
 
  private:
   //!
   void process() {
-    auto client = socket_.accept();
-    auto rcv = client.receive(tools::BUFFER_SIZE);
-    std::string cmd(rcv.data());
-    std::string response("Executing command [");
-    cmd.erase(cmd.end() - 1);
-    response += cmd;
-    response += "] ...\n";
-    client.send(response);
-    if (callback_) callback_(cmd, client);
+    try {
+      auto to_hash = tools::get_current_time_and_date();
+      auto id = tools::hash(to_hash);
+      auto new_client = std::make_shared<client>(socket_.accept());
+
+      clients_[id] = std::move(new_client);
+      auto rcv = clients_[id]->receive(tools::BUFFER_SIZE);
+      std::string cmd(rcv.data());
+      std::string response("Executing command [");
+      cmd.erase(cmd.end() - 1);
+      response += cmd;
+      response += "] ...\n";
+      clients_[id]->send(response);
+      if (callback_) callback_(cmd, clients_[id]);
+
+    } catch (const std::exception &e) {
+      __DISPLAY_ERROR__(e.what());
+      stop();
+    }
   }
 
  private:
@@ -342,8 +459,11 @@ class server {
   //! boolean to know if the server is already running
   std::atomic_bool is_running_;
 
+  // //! clients
+  std::map<std::size_t, std::shared_ptr<client>> clients_;
+
   //! server behaviour
-  std::function<void(const std::string &, socket &client)> callback_;
+  std::function<void(const std::string &, std::shared_ptr<client> &)> callback_;
 };
 
 }  //! namespace tcp
--- c++/server/main.cpp
+++ c++/server/main.cpp
@@ -5,16 +5,17 @@ using namespace netlib;
 int main() {
   network::tcp::server server;
 
-  auto callback = [](const std::string& cmd, network::tcp::socket& client) {
+  auto callback = [](const std::string& cmd,
+                     std::shared_ptr<network::tcp::client>& client) {
     int res = std::system(std::string(cmd).c_str());
 
-    client.send(not res ? "Command correctly executed.\n"
-                        : "Failed executing given command.\n");
+    client->send(not res ? "Command correctly executed.\n"
+                         : "Failed executing given command.\n");
 
-    client.close();
+    client->disconnect();
   };
 
   server.on_accept(callback);
-  server.run("127.0.0.1", 8888);
+  server.run("192.168.0.107", 8888);
   server.stop();
 }
