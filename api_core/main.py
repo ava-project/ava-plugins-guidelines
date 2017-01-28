ef2359e55260bc961eeb71c6091b3a08ad6a2bc9
Tommy <thomas.milox@epitech.eu>
2017-01-28 09:31:11 +0100

    [core] 	- update PNL (kappa) 	- update main.

--- api_core/main.py
+++ api_core/main.py
@@ -2,4 +2,6 @@ from PNL.tcp.server import server
 
 if __name__ == "__main__":
     s = server("127.0.0.1", 27017)
+    s.listen(50)
     print("server is running on ", s.host, ":", s.port)
+    s.run()
